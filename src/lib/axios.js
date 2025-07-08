4796b351a5bdd70578f80b7aaf2684ecb46e5ac4
sandeep14032004 <sandeeppati69@gmail.com>
2025-07-09 00:49:16 +0530

    changing base url

--- src/lib/axios.js
+++ src/lib/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-  baseURL: "http://localhost:5001/api",
+  baseURL: "https://nexchat-backend-ski0.onrender.com/",
   withCredentials: true,
 });
