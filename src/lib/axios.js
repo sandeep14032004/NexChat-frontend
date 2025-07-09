d78ef14217777aae4eccaff936c52c7537f95433
sandeep14032004 <sandeeppati69@gmail.com>
2025-07-09 09:34:19 +0530

    modified

--- src/lib/axios.js
+++ src/lib/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-  baseURL: "https://nexchat-backend-ski0.onrender.com/",
+  baseURL: "https://nexchat-backend-ski0.onrender.com/api",
   withCredentials: true,
 });
--- src/store/useAuthStore.js
+++ src/store/useAuthStore.js
@@ -4,7 +4,9 @@ import { axiosInstance } from "../lib/axios.js";
 import { io } from "socket.io-client";
 
 const BASE_URL =
-  import.meta.env.MODE === "development" ? "http://localhost:5001" : "/";
+  import.meta.env.MODE === "development"
+    ? "http://localhost:5001"
+    : "https://nexchat-backend.onrender.com";
 
 export const useAuthStore = create((set, get) => ({
   authUser: null,
